32f6983dba26be4e7ee80f76e3910174629ca326
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2022-04-15 01:43:28 +0530

    integrated store

---
+++ store/index.js
@@ -0,0 +1,180 @@
+// IMports
+import axios from 'axios'
+
+// Modules
+
+// States
+const state = {
+  broadcastEvents: [],
+  templates: [],
+  goals: [],
+}
+
+// Getters
+const getters = {
+  getBroadcastEvents: (state) => state.broadcastEvents,
+  getTemplates: (state) => state.templates,
+  getGoals: (state) => state.goals,
+}
+
+// Actions
+const actions = {
+  addNewBroadcast({ commit }, broadcastEvent) {
+    console.log('--- Adding new broadcast Event to the scheduler ---')
+    new Promise((resolve, reject) => {
+      axios({
+        url: `${process.env.baseUrl}/core/broadcast-event/create/`,
+        method: 'POST',
+        data: broadcastEvent,
+      })
+        .then(() => {
+          axios({
+            url: `${process.env.baseUrl}/core/broadcast-event/?size=20`,
+            method: 'GET',
+          })
+            .then((response) => {
+              if (response.data.count > 0) {
+                commit('FETCH_BROADCAST_EVENTS', response.data.results)
+                resolve(response.data.results)
+              }
+            })
+            .catch((err) => {
+              reject(err)
+            })
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  },
+  fetchBroadcastEvents({ commit }) {
+    console.log('--- fetching broadcast events - 20 at the moment--- ')
+    new Promise((resolve, reject) => {
+      axios({
+        url: `${process.env.baseUrl}/core/broadcast-event/?size=20`,
+        method: 'GET',
+      })
+        .then((response) => {
+          commit('FETCH_BROADCAST_EVENTS', response.data.results)
+          resolve(response.data.results)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  },
+  updateBroadcastEvent({ commit }, completeData) {
+    console.log('UPDATING BROADCAST EVENT --- ', completeData['data'])
+    let eventId = completeData['id']
+    let data = completeData['data']
+    new Promise((resolve, reject) => {
+      axios({
+        url: `${process.env.baseUrl}/core/broadcast-event/update/${eventId}/`,
+        method: 'PATCH',
+        data: data,
+      })
+        .then(() => {
+          axios({
+            url: `${process.env.baseUrl}/core/broadcast-event/?size=20`,
+            method: 'GET',
+          })
+            .then((response) => {
+              commit('FETCH_BROADCAST_EVENTS', response.data.results)
+              resolve(response.data.results)
+            })
+            .catch((err) => {
+              reject(err)
+            })
+        })
+        .catch((err) => reject(err))
+    })
+  },
+  deleteFolder({ commit }, eventId) {
+    console.log('DELETING BROADCAST EVENT --- ')
+    new Promise((resolve, reject) => {
+      axios({
+        url: `${process.env.baseUrl}/core/broadcast-event/update/${eventId}/`,
+        method: 'DELETE',
+      })
+        .then(() => {
+          axios({
+            url: `${process.env.baseUrl}/core/broadcast-event/?size=20`,
+            method: 'GET',
+          })
+            .then((response) => {
+              commit('FETCH_BROADCAST_EVENTS', response.data.results)
+              resolve(response.data.results)
+            })
+            .catch((err) => {
+              reject(err)
+            })
+        })
+        .catch((err) => reject(err))
+    })
+  },
+  fetchTemplates({ commit }) {
+    console.log('--- FETCHING TEMPLATES --- ')
+    new Promise((resolve, reject) => {
+      axios({
+        url: `${process.env.baseUrl}/core/template/?size=10`,
+        method: 'GET',
+      })
+        .then((response) => {
+          commit('FETCH_TEMPLATES', response.data.results)
+          resolve(response.data.results)
+        })
+        .catch((err) => reject(err))
+    })
+  },
+  fetchGoals({ commit }) {
+    console.log('--- FETCHING GOALS (10 at a time) --- ')
+    new Promise((resolve, reject) => {
+      axios({
+        url: `${process.env.baseUrl}/core/goal/?size=10`,
+        method: 'GET',
+      })
+        .then((response) => {
+          commit('FETCH_GOALS', response.data.results)
+          resolve(response.data.results)
+        })
+        .catch((err) => reject(err))
+    })
+  },
+  fetchUserCohorts({ commit }) {
+    console.log('--- FETCHING USER COHORTS (10 at a time) --- ')
+    new Promise((resolve, reject) => {
+      axios({
+        url: `${process.env.baseUrl}/core/user-cohort/?size=10`,
+        method: 'GET',
+      })
+        .then((response) => {
+          commit('FETCH_USER_COHORTS', response.data.results)
+          resolve(response.data.results)
+        })
+        .catch((err) => reject(err))
+    })
+  },
+}
+
+// Mutations
+const mutations = {
+  FETCH_BROADCAST_EVENTS(state, broadcastEvents) {
+    state.broadcastEvents = broadcastEvents
+  },
+  FETCH_TEMPLATES(state, templates) {
+    state.templates = templates
+  },
+  FETCH_GOALS(state, goals) {
+    state.goals = goals
+  },
+  FETCH_USER_COHORTS(state, cohorts) {
+    state.cohorts = cohorts
+  },
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+}
