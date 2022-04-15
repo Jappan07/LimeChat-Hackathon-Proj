9b4c81eb1755f73fdf9d9eb1698b9bbfe0a5edcc
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2022-04-15 15:38:24 +0530

    making suggested time component

---
+++ pages/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+      <suggested-time :broadcastSize="200000" :date="'2022-04-20'" />
+  </v-app>
+</template>
+
+<script>
+import SuggestedTime from '../components/common/SuggestedTime.vue'
+export default {
+  components: { SuggestedTime },
+
+}
+</script>
+
+<style>
+
+</style>
